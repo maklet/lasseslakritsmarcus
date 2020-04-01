1155197416129b80721b1077fbaec6e18301e4f7
maklet <makletlindfors@gmail.com>
2020-04-01 13:08:42 +0200

    heroku deploy

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
  
 const config = {
-    databaseURL: process.env.DATABASE,
-    //mail: process.env.MAIL
+    databaseURL: process.env.DATABASE
 }
  
 module.exports = config
