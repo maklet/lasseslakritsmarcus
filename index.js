1ebd86b336833f616f877a7b2a839bfd79e18e7d
maklet <makletlindfors@gmail.com>
2020-04-03 14:54:12 +0200

    fix

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
-app.set('views',  [path.join(__dirname, 'views'),path.join(__dirname, 'views/public')]);
+app.set('views',  [path.join(__dirname, 'views'),path.join(__dirname, 'views/public'),path.join(__dirname, 'views/userprofile')]);
 
 app.use(lassesLakritsRouter);
 app.use(admin);
